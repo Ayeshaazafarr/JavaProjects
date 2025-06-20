2f78f14a188c3943a7a75e5a936fe254541ed34b
Ayeshaazafarr <ayeshazafarr.0@gmail.com>
2025-06-20 18:00:58 +0500

    lab 4 tasks

---
+++ lab4/src/lab4/Main.java
@@ -0,0 +1,20 @@
+package lab4;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		 Employee emp = new Employee();
+	        
+	     
+	        
+	        System.out.println("Bonus for base salary (int): " + emp.calculateBonus(50000));
+	        
+	       
+	        System.out.println("Bonus for base salary (double) and rating 5: " + emp.calculateBonus(60000, 3));
+	      ;
+	    
+	
+	     
+}
+}
